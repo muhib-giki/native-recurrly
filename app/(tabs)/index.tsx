1be8cb4dc30a0dba8a0fc873b20e6b3cdd963062
muhib-giki <muhibmhanif@gmail.com>
2026-04-05 09:22:56 +0500

    tabs designed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,18 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
- 
+import { Text } from "react-native";
+
+import { styled } from 'nativewind';
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text className="text-7xl font-bold text-success">
+        Home
       </Text>
       <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Go To Onboarding Screen</Link>
-    </View>
+    </SafeAreaView> 
   );
 }
