0ab9d40349448b54f48775a2f32949c3ebd21b3c
muhib-giki <muhibmhanif@gmail.com>
2026-04-08 06:02:24 +0500

    fixed an issue

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,7 +9,7 @@ export const formatCurrency = (value: number, currency: string = "USD") => {
             maximumFractionDigits: 2,
         }).format(value);
     } catch (error) {
-        return value.toFixed(2);;
+        return value.toFixed(2);
     }
 }
 
